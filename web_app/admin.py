38877f4ee3b4f0f366472e13d42786978a4c56c3
Altynai <urmatovnaa05@gmail.com>
2022-08-16 13:55:48 +0600

    add key words

--- web_app/admin.py
+++ web_app/admin.py
@@ -1,20 +1,12 @@
 from django.contrib import admin
 
-from web_app.models import ProductPhoto, ProductName, Company
-
-
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ('name', 'company')
-
-
-class PersonAdmin2(admin.ModelAdmin):
-    list_display = ('company', 'photo')
+from web_app.models import ProductPhoto, Company
 
 
 class PersonAdmin3(admin.ModelAdmin):
     list_display = ('name', 'web_site')
 
 
-admin.site.register(ProductName, PersonAdmin)
-admin.site.register(ProductPhoto, PersonAdmin2)
+admin.site.register(ProductPhoto)
 admin.site.register(Company, PersonAdmin3)
+
