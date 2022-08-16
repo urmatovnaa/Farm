ffa41295975e57885ca5c5bcbb2c68e3eb8286b7
Altynai <urmatovnaa05@gmail.com>
2022-08-16 13:55:48 +0600

    add key words

--- web_app/serializers.py
+++ web_app/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from web_app.models import ProductPhoto, Company, ProductName
+from web_app.models import ProductPhoto, Company
 
 
 class ProductPhotoSerializer(serializers.ModelSerializer):
@@ -9,24 +9,16 @@ class ProductPhotoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ProductNameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductName
-        fields = '__all__'
-
-
 class CompanySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Company
-        fields = ('id', 'name', 'logo')
+        fields = ('id', 'name', 'photo')
 
 
 class CompanyDetailSerializer(serializers.ModelSerializer):
     products_photos = ProductPhotoSerializer(many=True)
-    products_name = ProductNameSerializer(many=True)
 
     class Meta:
         model = Company
-        fields = ('id', 'name', 'logo', 'information', 'web_site', 'products_name', 'products_photos')
-
+        fields = ('id', 'name', 'logo', 'information', 'web_site', 'key_words', 'products_photos')
